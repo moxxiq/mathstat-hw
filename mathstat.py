c13b958ddff13f5fa8a577844ffef99d85f0a34f
moxxiq <dimanavsisto@gmail.com>
2020-06-07 03:33:03 +0300

    4 HW done

--- mathstat.py
+++ mathstat.py
@@ -1,13 +1,13 @@
 import math, functools
 from collections import Counter
 
-sample_mean = lambda data: sum(data)/len(data)
+calc_sample_mean = lambda data: sum(data)/len(data)
 
-sample_mean_squares =  lambda data: sum(map(lambda x: x*x,data))/len(data)
+calc_sample_mean_squares =  lambda data: sum(map(lambda x: x*x,data))/len(data)
 
-variance = lambda data: sample_mean_squares(data)-sample_mean(data)**2
+calc_variance = lambda data: calc_sample_mean_squares(data)-calc_sample_mean(data)**2
 
-corrected_variance = lambda data: len(data)*variance(data)/(len(data)-1)
+correct_variance = lambda var, n: n*var/(n-1)
 
-median = lambda data: (data[len(data)//2]+data[len(data)//2+1])/2 if len(data)%2 else data[(len(data)+1)//2];
+calc_median = lambda data: (data[len(data)//2]+data[len(data)//2+1])/2 if len(data)%2 else data[(len(data)+1)//2];
 
